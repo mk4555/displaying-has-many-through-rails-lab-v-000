abc9738cfd822de0c92d6f53d7bc89cdf22d5199
Min Kim <mk4555@nyu.edu>
2018-02-17 00:52:29 +0000

    automatically backed up by learn

--- app/controllers/doctor_controller.rb
+++ app/controllers/doctor_controller.rb
@@ -1,6 +1,7 @@
 class DoctorController < ApplicationController
   def index
   end
+  
   def show
   end
 end
--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,4 +1,5 @@
 class PatientController < ApplicationController
+  def indexend
   def show
   end
 end
